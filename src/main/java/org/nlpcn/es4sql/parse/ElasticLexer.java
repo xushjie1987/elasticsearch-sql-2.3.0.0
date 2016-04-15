909849262b2c66d373ed1601e5bce35ffe0c9c9c
Xu Sheng Jie <xushengjie@oneapm.com>
2016-04-15 10:37:53 +0800

    commit 1

--- src/main/java/org/nlpcn/es4sql/parse/ElasticLexer.java
+++ src/main/java/org/nlpcn/es4sql/parse/ElasticLexer.java
@@ -1,12 +1,12 @@
 package org.nlpcn.es4sql.parse;
 
-import com.alibaba.druid.sql.dialect.mysql.parser.MySqlLexer;
-import com.alibaba.druid.sql.parser.ParserException;
-import com.alibaba.druid.sql.parser.Token;
-
 import static com.alibaba.druid.sql.parser.CharTypes.isFirstIdentifierChar;
 import static com.alibaba.druid.sql.parser.CharTypes.isIdentifierChar;
 import static com.alibaba.druid.sql.parser.LayoutCharacters.EOI;
+
+import com.alibaba.druid.sql.dialect.mysql.parser.MySqlLexer;
+import com.alibaba.druid.sql.parser.ParserException;
+import com.alibaba.druid.sql.parser.Token;
 
 /**
  * Created by Eliran on 18/8/2015.
@@ -15,23 +15,27 @@ public class ElasticLexer extends MySqlLexer {
     public ElasticLexer(String input) {
         super(input);
     }
-
-
-    public ElasticLexer(char[] input, int inputLength, boolean skipComment) {
-        super(input, inputLength, skipComment);
+    
+    public ElasticLexer(char[] input,
+                        int inputLength,
+                        boolean skipComment) {
+        super(input,
+              inputLength,
+              skipComment);
     }
-
+    
+    @Override
     public void scanIdentifier() {
         final char first = ch;
-
+        
         if (ch == '`') {
-
+            
             mark = pos;
             bufPos = 1;
             char ch;
             for (;;) {
                 ch = charAt(++pos);
-
+                
                 if (ch == '`') {
                     bufPos++;
                     ch = charAt(++pos);
@@ -39,14 +43,15 @@ public class ElasticLexer extends MySqlLexer {
                 } else if (ch == EOI) {
                     throw new ParserException("illegal identifier");
                 }
-
+                
                 bufPos++;
                 continue;
             }
-
+            
             this.ch = charAt(pos);
-
-            stringVal = subString(mark, bufPos);
+            
+            stringVal = subString(mark,
+                                  bufPos);
             Token tok = keywods.getKeyword(stringVal);
             if (tok != null) {
                 token = tok;
@@ -54,28 +59,28 @@ public class ElasticLexer extends MySqlLexer {
                 token = Token.IDENTIFIER;
             }
         } else {
-
+            
             final boolean firstFlag = isFirstIdentifierChar(first);
             if (!firstFlag) {
                 throw new ParserException("illegal identifier");
             }
-
+            
             mark = pos;
             bufPos = 1;
             char ch;
             for (;;) {
                 ch = charAt(++pos);
-
+                
                 if (!isElasticIdentifierChar(ch)) {
                     break;
                 }
-
+                
                 bufPos++;
                 continue;
             }
-
+            
             this.ch = charAt(pos);
-
+            
             stringVal = addSymbol();
             Token tok = keywods.getKeyword(stringVal);
             if (tok != null) {
@@ -85,9 +90,13 @@ public class ElasticLexer extends MySqlLexer {
             }
         }
     }
-
-
+    
     private boolean isElasticIdentifierChar(char ch) {
-        return ch == '*' || ch == ':' || ch == '-'  || ch == '.' || ch == ';' || isIdentifierChar(ch);
+        return ch == '*' ||
+               ch == ':' ||
+               ch == '-' ||
+               ch == '.' ||
+               ch == ';' ||
+               isIdentifierChar(ch);
     }
 }
--- src/main/java/org/nlpcn/es4sql/parse/ElasticSqlExprParser.java
+++ src/main/java/org/nlpcn/es4sql/parse/ElasticSqlExprParser.java
@@ -1,5 +1,7 @@
 package org.nlpcn.es4sql.parse;
 
+import java.util.List;
+
 import com.alibaba.druid.sql.ast.SQLCommentHint;
 import com.alibaba.druid.sql.ast.SQLExpr;
 import com.alibaba.druid.sql.ast.expr.SQLExistsExpr;
@@ -10,8 +12,6 @@ import com.alibaba.druid.sql.parser.Lexer;
 import com.alibaba.druid.sql.parser.ParserException;
 import com.alibaba.druid.sql.parser.Token;
 
-import java.util.List;
-
 /**
  * Created by Eliran on 18/8/2015.
  */
@@ -19,12 +19,12 @@ public class ElasticSqlExprParser extends MySqlExprParser {
     public ElasticSqlExprParser(Lexer lexer) {
         super(lexer);
     }
-
+    
     public ElasticSqlExprParser(String sql) {
         this(new ElasticLexer(sql));
         this.lexer.nextToken();
     }
-
+    
     @SuppressWarnings({ "unchecked", "rawtypes" })
     @Override
     public void parseHints(List hints) {
@@ -33,70 +33,75 @@ public class ElasticSqlExprParser extends MySqlExprParser {
             lexer.nextToken();
         }
     }
-
+    
     @Override
     public SQLExpr primary() {
-
-        if(lexer.token() == Token.LBRACE){
+        
+        if (lexer.token() == Token.LBRACE) {
             lexer.nextToken();
             boolean foundRBrace = false;
-            if(lexer.stringVal().equals("ts")){
+            if (lexer.stringVal()
+                     .equals("ts")) {
                 String current = lexer.stringVal();
                 do {
-                    if(current.equals(lexer.token().RBRACE.name())){
+                    if (current.equals(lexer.token().RBRACE.name())) {
                         foundRBrace = true;
                         break;
                     }
                     lexer.nextToken();
-                    current = lexer.token().name();
-                }while(!foundRBrace && !current.trim().equals(""));
-
-                if(foundRBrace){
+                    current = lexer.token()
+                                   .name();
+                } while (!foundRBrace && !current.trim()
+                                                 .equals(""));
+                
+                if (foundRBrace) {
                     SQLOdbcExpr sdle = new SQLOdbcExpr(lexer.stringVal());
-
+                    
                     accept(Token.RBRACE);
                     return sdle;
-                }else{
+                } else {
                     throw new ParserException("Error. Unable to find closing RBRACE");
                 }
-            }else{
+            } else {
                 throw new ParserException("Error. Unable to parse ODBC Literal Timestamp");
             }
-        }
-        else if(lexer.token() == Token.LBRACKET){
+        } else if (lexer.token() == Token.LBRACKET) {
             StringBuilder identifier = new StringBuilder();
             lexer.nextToken();
             String prefix = "";
-            while(lexer.token()!=Token.RBRACKET){
-                if(lexer.token() != Token.IDENTIFIER && lexer.token()!=Token.INDEX && lexer.token()!=Token.LITERAL_CHARS){
-                    throw new ParserException("All items between Brackets should be identifiers , got:" +lexer.token());
+            while (lexer.token() != Token.RBRACKET) {
+                if (lexer.token() != Token.IDENTIFIER &&
+                    lexer.token() != Token.INDEX &&
+                    lexer.token() != Token.LITERAL_CHARS) {
+                    throw new ParserException("All items between Brackets should be identifiers , got:" +
+                                              lexer.token());
                 }
                 identifier.append(prefix);
                 identifier.append(lexer.stringVal());
                 prefix = " ";
                 lexer.nextToken();
             }
-
+            
             accept(Token.RBRACKET);
             return new SQLIdentifierExpr(identifier.toString());
-        }
-        else if (lexer.token() == Token.NOT) {
+        } else if (lexer.token() == Token.NOT) {
             lexer.nextToken();
             SQLExpr sqlExpr;
             if (lexer.token() == Token.EXISTS) {
                 lexer.nextToken();
                 accept(Token.LPAREN);
-                sqlExpr = new SQLExistsExpr(createSelectParser().select(), true);
+                sqlExpr = new SQLExistsExpr(createSelectParser().select(),
+                                            true);
                 accept(Token.RPAREN);
             } else if (lexer.token() == Token.LPAREN) {
                 lexer.nextToken();
-
+                
                 SQLExpr notTarget = expr();
-
+                
                 accept(Token.RPAREN);
-
+                
                 sqlExpr = new SQLNotExpr(notTarget);
-
+                
                 return primaryRest(sqlExpr);
             } else {
                 SQLExpr restExpr = relational();
@@ -104,16 +109,16 @@ public class ElasticSqlExprParser extends MySqlExprParser {
             }
             return sqlExpr;
         }
-
+        
         boolean parenWrapped = lexer.token() == Token.LPAREN;
-
+        
         SQLExpr expr = super.primary();
-
+        
         // keep track of if the identifier is wrapped in parens
         if (parenWrapped && expr instanceof SQLIdentifierExpr) {
             expr = new SQLParensIdentifierExpr((SQLIdentifierExpr) expr);
         }
-
+        
         return expr;
     }
 }
